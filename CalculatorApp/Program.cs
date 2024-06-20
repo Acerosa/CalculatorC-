a868c9c40c79e0e13b905449252f00b69425b834
ricardorosa <rjrosa@outlook.com>
2024-06-20 12:32:20 +0100

    getting number functionality

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -7,6 +7,19 @@ namespace Calculator
 static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the calculator program");
+            int num1 = GetNumber("Enter your first number:");
+            int num2 = GetNumber("Enter your second number:");
+        }
+
+        static int GetNumber(string prompt)
+        {
+            int number;
+            Console.WriteLine(prompt);
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+            return number;
         }
     }
 }

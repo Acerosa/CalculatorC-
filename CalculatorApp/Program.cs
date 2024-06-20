2acd70b56f2007d54c67811842a0ba6ff3e25a3e
ricardorosa <rjrosa@outlook.com>
2024-06-20 12:40:17 +0100

    get opperation functionality

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -9,6 +9,11 @@ static void Main(string[] args)
             Console.WriteLine("Welcome to the calculator program");
             int num1 = GetNumber("Enter your first number:");
             int num2 = GetNumber("Enter your second number:");
+            string operation = GetOperation();
+            
+
+
+            Console.ReadKey();
         }
 
         static int GetNumber(string prompt)
@@ -21,5 +26,25 @@ static void Main(string[] args)
             }
             return number;
         }
+
+        static string GetOperation()
+        {
+            Console.WriteLine("Please choose your desired operation:");
+            Console.WriteLine("For addition choose (+),\nFor subtraction choose (-),\nFor division choose (/),\nFor multiplication choose (*)");
+
+            string operation;
+            while (true)
+            {
+                operation = Console.ReadLine();
+                if (operation == "+" || operation == "-" || operation == "/" || operation == "*")
+                {
+                    break;
+                }
+                Console.WriteLine("Invalid operation. Please choose a valid operation (+, -, /, *).");
+            }
+            return operation;
+        }
+
+
     }
 }
